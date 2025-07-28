665d88debe7b245fcc548af2329a699d05fb491d
David Umoru <umorudavido@gmail.com>
2025-07-28 10:48:30 +0100

    Remove mail and update footer description

--- components/FooterLeft.tsx
+++ components/FooterLeft.tsx
@@ -1,8 +1,7 @@
 export default function FooterLeft() {
   return (
     <div className="absolute bottom-0 left-0 p-6 text-gray-500 text-sm font-medium">
-      <div>UI & Design Projects</div>
-      <div className="opacity-80">Experimentation & Inspiration</div>
+      <div>Creative Coding Projects</div>
     </div>
   );
 }
