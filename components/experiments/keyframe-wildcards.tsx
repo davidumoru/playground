d5ca92e7661a0d48ec932109d273b886232512ae
David Umoru <umorudavido@gmail.com>
2025-12-14 21:38:22 +0100

    Remove unused experimental components

--- components/experiments/keyframe-wildcards.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-export default function KeyframeWildcards() {
-  return (
-    <div className="flex items-center justify-center">
-      <div className="relative size-32 rounded-2xl bg-accent animate-pulse" />
-    </div>
-  )
-}
